770252decbba5c60b789f204d4ca928e7662f943
Ana Machado <ana.machado8002@gmail.com>
2026-08-09 15:02:43 -0300

    feat: add problems 1046 and 1087

---
+++ ecox21/lista01/ex1046.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main(void) {
+	int start, end;
+	int duration;
+
+	scanf("%d %d", &start, &end);
+
+	if (start > end)
+		duration = 24 - start + end;
+	else
+		duration = end - start;
+
+	printf("O JOGO DUROU %d HORA(S)\n", duration);
+
+	return 0;
+}
